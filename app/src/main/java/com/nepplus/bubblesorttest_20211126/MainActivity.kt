33e8cdf301f98fc974e869288567a70b8c6c7e1c
이아현 <suk03029@gmail.com>
2021-11-26 12:21:36 +0900

    코드 짰는데 오류 남

--- app/src/main/java/com/nepplus/bubblesorttest_20211126/MainActivity.kt
+++ app/src/main/java/com/nepplus/bubblesorttest_20211126/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.bubblesorttest_20211126
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.nepplus.bubblesorttest_20211126.databinding.ActivityMainBinding
 
@@ -9,12 +10,34 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    val mList = arrayListOf(3, 5, 2, 1, 4, 9, 8)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnCheckNum.setOnClickListener {
+
+            for (num in mList) {
+
+                Log.d("숫자", num.toString())
+
+                for (i in 0 until mList.size) {
+                    var tempNum = mList[i]
 
 
+                    if (mList[i] >= mList[i + 1]) {
+                        tempNum = mList[i]
+                        mList[i] = mList[i + 1]
+                        mList[i + 1] = tempNum
+                        }
+                    }
+                Log.d("숫자", num.toString())
+
+            }
+
+        }
 
 
     }
-    }
+}
